db0272b8eea99720e68e4bc6a688ff3e904c901f
Nikita Kozelko <nikitakozelko@gmail.com>
2017-12-03 03:46:50 +0300

    View for project.

---
+++ View/BatteryViewer.Designer.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections.Generic;
+
+namespace Battery
+{
+    partial class BatteryViewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.batteryListBox = new System.Windows.Forms.ListBox();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.PowerTimeoutLabel = new System.Windows.Forms.Label();
+            this.TimeDisplaComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // batteryListBox
+            // 
+            this.batteryListBox.FormattingEnabled = true;
+            this.batteryListBox.ItemHeight = 20;
+            this.batteryListBox.Location = new System.Drawing.Point(62, 18);
+            this.batteryListBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.batteryListBox.Name = "batteryListBox";
+            this.batteryListBox.Size = new System.Drawing.Size(376, 84);
+            this.batteryListBox.TabIndex = 0;
+            this.batteryListBox.SelectedIndexChanged += new System.EventHandler(this.batteryListBox_SelectedIndexChanged);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(168, 249);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(134, 35);
+            this.exitButton.TabIndex = 1;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // PowerTimeoutLabel
+            // 
+            this.PowerTimeoutLabel.AutoSize = true;
+            this.PowerTimeoutLabel.Location = new System.Drawing.Point(164, 132);
+            this.PowerTimeoutLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.PowerTimeoutLabel.Name = "PowerTimeoutLabel";
+            this.PowerTimeoutLabel.Size = new System.Drawing.Size(138, 20);
+            this.PowerTimeoutLabel.TabIndex = 3;
+            this.PowerTimeoutLabel.Text = "Set power timeout";
+            // 
+            // TimeDisplaComboBox
+            // 
+            this.TimeDisplaComboBox.FormattingEnabled = true;
+            this.TimeDisplaComboBox.Items.AddRange(new object[] {
+            "1",
+            "5",
+            "10",
+            "15",
+            "20",
+            "30",
+            "60"});
+            this.TimeDisplaComboBox.Location = new System.Drawing.Point(168, 183);
+            this.TimeDisplaComboBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.TimeDisplaComboBox.Name = "TimeDisplaComboBox";
+            this.TimeDisplaComboBox.Size = new System.Drawing.Size(132, 28);
+            this.TimeDisplaComboBox.TabIndex = 5;
+            this.TimeDisplaComboBox.SelectedIndexChanged += new System.EventHandler(this.TimeDisplaComboBox_SelectedIndexChanged);
+            // 
+            // BatteryViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 309);
+            this.Controls.Add(this.TimeDisplaComboBox);
+            this.Controls.Add(this.PowerTimeoutLabel);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.batteryListBox);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "BatteryViewer";
+            this.Text = "BatteryViewer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.BatteryViewer_FormClosing);
+            this.Load += new System.EventHandler(this.BatteryViewer_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox batteryListBox;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label PowerTimeoutLabel;
+        private System.Windows.Forms.ComboBox TimeDisplaComboBox;
+    }
+}
+
---
+++ View/BatteryViewer.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.Threading;
+
+namespace Battery
+{
+    public partial class BatteryViewer : Form
+    {
+        private BatteryApi batteryApi;
+
+        private Thread batteryConditionUpdater;
+
+        private bool isBatteryConditionUpdaterContinueWork;
+
+        private int batteryConditionUpdatingFrequency = 2000;
+
+        private const int DEFAULT_POWER_TIMEOUT_IN_MINUTES = 60;
+
+        public BatteryViewer()
+        {
+            InitializeComponent();
+        }
+
+        private void BatteryViewer_Load(object sender, EventArgs e)
+        {
+            batteryApi = BatteryApi.GetInstance();
+            
+            batteryApi.SetPowerTimeout(60);
+            isBatteryConditionUpdaterContinueWork = true;
+            batteryConditionUpdater = new Thread(UpdateBatteryConditIonProcedure);
+            batteryConditionUpdater.Start();
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            TurnOffBatteryConditionUpdater();
+            SetDefaultPowerTimeout();
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void UpdateBatteryConditIonProcedure()
+        {
+            while (isBatteryConditionUpdaterContinueWork)
+            {
+                var batteryCondition = batteryApi.GetCurrentBatteryCondition();
+                UpdateBatteryCondition(batteryCondition);
+                Thread.Sleep(batteryConditionUpdatingFrequency);
+            }
+        }
+
+        // This delegate enables asynchronous calls for setting
+        // updated battery condition on BatteryListBox.
+        delegate void UpdateBatteryConditionCallBack(BatteryCondition batteryCondition);
+
+        private void UpdateBatteryCondition(BatteryCondition batteryCondition)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.batteryListBox.InvokeRequired)
+            {
+                UpdateBatteryConditionCallBack callBack
+                    = new UpdateBatteryConditionCallBack(UpdateBatteryCondition);
+                this.Invoke(callBack, new object[] { batteryCondition});
+            }
+            else
+            {
+                SetUpdatedBatteryCondition(batteryCondition);
+            }
+        }
+
+        private void SetUpdatedBatteryCondition(BatteryCondition batteryCondition) 
+        {
+            batteryListBox.Items.Clear();
+            batteryListBox.Items.Add($"Battery plug type: \t\t{batteryCondition.PlugType}");
+            batteryListBox.Items.Add($"Battery charge status: \t{batteryCondition.ChargeStatus}");
+            batteryListBox.Items.Add($"Battery estimated runtime: \t{batteryCondition.EstimatedRunTime}");
+        }
+
+        private void TurnOffBatteryConditionUpdater()
+        {
+            isBatteryConditionUpdaterContinueWork = false;
+            batteryConditionUpdater.Abort();
+        }
+
+        private void SetDefaultPowerTimeout()
+        {
+            batteryApi.SetPowerTimeout(DEFAULT_POWER_TIMEOUT_IN_MINUTES); 
+        }
+
+        private void BatteryViewer_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            TurnOffBatteryConditionUpdater();
+            SetDefaultPowerTimeout();
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void TimeDisplaComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedValue = Int32.Parse(TimeDisplaComboBox.SelectedItem.ToString());
+            batteryApi.SetPowerTimeout(selectedValue);
+        }
+
+        private void batteryListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
